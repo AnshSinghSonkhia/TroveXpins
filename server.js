6636702d69569c48263f38b7171457864b299ffe
Ansh Singh Sonkhia <110414565+AnshSinghSonkhia@users.noreply.github.com>
2024-02-25 10:02:19 +0530

    resolved bugs

--- server.js
+++ server.js
@@ -6,5 +6,5 @@ const port = process.env.PORT || 5000;
 const server = http.createServer(app);
 
 server.listen(port, () => {
-    console.og('Started on port ${port}');
+    console.log('Started on port ${port}');
 });
